df59431dd2f035ef52672ab758ba17cd94116ad2
ocedev112 <141877303+ocedev112@users.noreply.github.com>
2025-03-24 12:00:02 +0100

    Update electionRoute.js

--- Backend/routes/electionRoute.js
+++ Backend/routes/electionRoute.js
@@ -1,17 +1,12 @@
 const express = require("express");
+const {createElection, adminLogin , addCandidate, getElection} = require("../controllers/electionController")
+const upload = require('../middleware/multer')
 const router = express.Router();
-const Election = require("../models/Election")
 
-router.get("/", async (req,res) =>{
-    const elections = await Election.find();
-    res.json(elections)
-})
 
-router.post("/create", async (req, res) =>{
-    const {name, duration} = req.body;
-    const election = new Election({name, duration, isActive})
-    await election.save();
-    res.json({success: true, election});
-})
+router.post("/create", createElection)
+router.post("/adminLogin", adminLogin)
+router.post("/add-candidate", upload.single('photo') , addCandidate)
+router.get("/results/:electionId", getElection)
 
 module.exports = router
