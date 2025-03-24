848fb053ef0e1c4ef850322388b8d2b3da4d9917
ocedev112 <141877303+ocedev112@users.noreply.github.com>
2025-03-24 12:00:59 +0100

    Update Election.js

--- Backend/models/Election.js
+++ Backend/models/Election.js
@@ -3,10 +3,26 @@ const mongoose = require("mongoose")
 
 
 const ElectionSchema = new mongoose.Schema({
-    name: String,
-    startTime: Number,
-    duration: Number,
-    candidate: [{type: mongoose.Schema.Types.ObjectId, ref: "Election"}]
+    adminFistName: {type: String},
+    adminLastName: {type: String},
+    adminName: {type: String},
+    email: {type: String, required: true},
+    password: {type: String, required: true, minlength: 6},
+    confirmPassword: {type: String},
+    electionName: { type: String,  required: true},
+    electionId: {type: String, required: true},
+    days: {type: Number },
+    hours: {type: Number},
+    minutes: {type: Number},
+    seconds: {type: Number},
+    duration: {type: Number, required: true },
+    candidate: [{ 
+        candidateId: {type: Number, required: true},
+        name: {type: String, required: true},
+        post: {type: String, required: true},
+        manifesto: {type: String},
+        photo: {type: String}
+    }]
 })
 
 module.exports = mongoose.model("Election", ElectionSchema);
