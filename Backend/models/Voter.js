a596593973a12d963f2dbe66ff063bd9cd2ea29a
ocedev112 <141877303+ocedev112@users.noreply.github.com>
2025-03-24 12:02:02 +0100

    Update Voter.js

--- Backend/models/Voter.js
+++ Backend/models/Voter.js
@@ -1,12 +1,44 @@
 const mongoose = require("mongoose");
-
+const bcrypt = require('bcrypt');
+const { Wallet } = require("web3");
 
 const VoterSchema = new mongoose.Schema({
-    name: String,
-    email: String,
-    password: String,
-    votedForPost: {type: Map, of: Boolean},
+    electionId:{type: Number, required: true },
+    firstName: { type: String},
+    lastName: { type: String},
+    voterName: { type: String},
+    email:{type: String, required: true},
+    password:{type: String, required: true, minlength: 6}, 
+    confirmPassword:{type: String},
+    walletAddress: {
+        type: String,
+      
+    },
+    walletPrivateKey: {
+        type: String
+       
+    },
+    photo:{
+        type: String
+    },
+    joinedElections : [{
+         electionId: {type: String},
+         electionName: {type: String},
+         duration: {type: Number},
+         candidate: [{
+            candidateId: {type: Number},
+            name: {type: String},
+            post: {type: String},
+            manifesto: {type: String},
+            photo: {type: String}
+         }]
+
+    }],
+    votedForPost: {type: Boolean, default: false},
+
 })
+
+
 
 
 module.exports = mongoose.model("Voter", VoterSchema)
